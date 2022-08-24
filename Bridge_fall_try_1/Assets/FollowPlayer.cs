1ef8ce72b3870f99d0520c1284226c630353d479
axcel342 <mominimran000@gmail.com>
2022-08-24 19:21:10 +0500

    Improved movement using character controller and improved camera angle

--- Bridge_fall_try_1/Assets/FollowPlayer.cs
+++ Bridge_fall_try_1/Assets/FollowPlayer.cs
@@ -5,10 +5,18 @@ public class FollowPlayer : MonoBehaviour
 
     public Transform player;
     public Vector3 offset;
+    public Quaternion Camera_rotation;
 
     // Update is called once per frame
     void Update()
     {
+        //Camera_rotation.x = 28.408f;
+        //Camera_rotation.y = 180.283f;
+        //Camera_rotation.x = -0.021f;
+
         transform.position = player.position + offset;
+        //transform.rotation.eulerAngles();
+
+       
     }
 }
--- Bridge_fall_try_1/Assets/PlayerMotor.cs
+++ Bridge_fall_try_1/Assets/PlayerMotor.cs
@@ -5,59 +5,136 @@ using UnityEngine;
 public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
-    // Start is called before the first frame update
 
-    public float speed = 5.0f;
+    public float forwardspeed = 10.0f;
+    public float dragSpeed = 5.0f;
     private Vector3 moveVector;
-    private Vector3 turnVector;
-    
+    private float gravity = 12.0f;
+    private float verticalVelocity = 0.0f;
 
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        turnVector.z = 0;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //moveVector = moveVector = Vector3.zero;
-        controller.Move(Vector3.forward * speed * Time.deltaTime  );
+        moveVector = Vector3.zero;
 
-        Movement();
+        if (controller.isGrounded)
+        {
+            verticalVelocity = 0.0f;
+        }
+
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+
+        Movement_With_moveVector();
+
+        //Movement_3rd_try();
+
+        moveVector.y = verticalVelocity;
+
+        moveVector.z = forwardspeed;
+
+
+        controller.Move(moveVector * Time.deltaTime);
     }
 
-    void Movement()
+    //void Movement()
+    //{
+    //    if (Input.touchCount > 0)
+    //    {
+    //        Touch touch = Input.GetTouch(0);
+    //        Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+    //        touchPosition.z = 0f;
+            
+    //        turnVector.y = 0;
+    //        // transform.position = touch.position;
+
+    //        if (touch.position.x < (float)(Screen.width / 2.0f))
+    //        {
+                
+    //            turnVector.x = touchPosition.x;
+    //            turnVector.z = speed;
+    //            //rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+    //            //controller.Move(turnVector * Time.deltaTime * speed);
+    //            controller.Move(Vector3.right * Time.deltaTime * speed);
+    //            print("Right press");
+    //        }
+
+    //        if (touch.position.x > (float)(Screen.width / 2.0f))
+    //        {
+    //            turnVector.x = touchPosition.x;
+    //            controller.Move(Vector3.left * Time.deltaTime * speed);
+    //            //rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+    //            print("Left press");
+    //        }
+
+    //        print(touch.position.x);
+    //    }
+    //}
+
+    void Movement_With_moveVector()
     {
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
             Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
             touchPosition.z = 0f;
-            
-            turnVector.y = 0;
-            // transform.position = touch.position;
+
 
             if (touch.position.x < (float)(Screen.width / 2.0f))
             {
-                
-                turnVector.x = touchPosition.x;
-                turnVector.z = speed;
-                //rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
-                controller.Move(turnVector * Time.deltaTime * speed);
-                print("Right press");
+
+
+                moveVector.x = Vector3.right.x * dragSpeed;
+                print("Left press");
             }
 
             if (touch.position.x > (float)(Screen.width / 2.0f))
             {
-                turnVector.x = touchPosition.x;
-                controller.Move(Vector3.left * Time.deltaTime * speed);
-                //rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
-                print("Left press");
+                moveVector.x = Vector3.left.x * dragSpeed;
+
+                print("Right press");
             }
 
             print(touch.position.x);
+        }
+    }
+
+    void Movement_3rd_try()
+    {
+        for(int i = 0; i < Input.touchCount; i++)
+        {
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+
+            moveVector = Vector3.zero;
+
+            if (controller.isGrounded)
+            {
+                verticalVelocity = 0.0f;
+            }
+
+            else
+            {
+                verticalVelocity -= gravity * Time.deltaTime;
+            }
+
+            moveVector.x = Input.touches[i].position.x * dragSpeed;
+
+            moveVector.y = verticalVelocity;
+
+            moveVector.z = dragSpeed;
+
+
+            controller.Move(moveVector * Time.deltaTime);
+
         }
     }
 }
